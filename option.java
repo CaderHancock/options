f12852ab985b15379c9ed3d75f466266e128b656
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-01 19:01:34 +0000

    formatting and comments

--- option.java
+++ option.java
@@ -1,27 +1,40 @@
 /*
-//Object class used to do stuff with Options contracts
-//Contructed using a string containing the OCC Option of the contract https://en.wikipedia.org/wiki/Option_symbol
-//Written by Cader.Hancock@gmail.com
-//
-//TODO
-//Write methods to make api pulls
-//"              " calculate greeks and IV
-//"              " get values
+*Object class used to do stuff with Options contracts
+* @author Cader.Hancock@gmail.com
+*
+*TODO
+*Write methods to make api pulls
+*"              " calculate greeks and IV
+*"              " get values
 **/
 package cader.options;
 
+
+
 public class option{
 
-	public String contractSymbol, underlyingSymbol;
-	public double strikePrice;
+	public final String contractSymbol, underlyingSymbol;
+	public final double strikePrice;
 	private double mark, bid, ask, change, percentChange, volume, openInterest, delta, theta, gamma, vega, rho, iv;
 	
-	//test value
-	//contractSymbol = "SPY190401C00271000";
-
-
+	/**
+	* @param incomingSymbol The OCC symbol for the options contract represented by this class
+	* @see <a href="https://en.wikipedia.org/wiki/Option_symbol">Options symbol</a>
+	*/
 	public option(String incomingSymbol){
-		underlyingSymbol = incomingSymbol.substring(0,(incomingSymbol.length()-16));
+		contractSymbol = incomingSymbol; 
+		underlyingSymbol = incomingSymbol.substring(0,(incomingSymbol.length()-17));
 		
 	}
+	/** @return  3 element array of type double containing the contract's bid, ask, and mark prices */
+	public double[] refreshQuote(){
+		
+		//TODO API calls and stuff
+		double[] quote = new double[3];
+		quote[0] = bid;
+		quote[1] = ask;
+		quote[2] = mark;	
+		return quote;
+	}
+
 }
