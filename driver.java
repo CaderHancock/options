7dade534cccc35630bbaf493a3a71173a6b9ed6a
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-01 20:22:09 +0000

    moved files and made package directory structure

--- driver.java
+++ driver.java
@@ -1,9 +1,14 @@
-package cader.options; 
+//package options; 
+import options.option;
 
 public class driver{
 
 public static void main(String[] args){
 	option o = new option("SPY190401C00271000");
 	System.out.println(o.underlyingSymbol);
+	System.out.println(o.strikePrice);
+	//	String s = "SPY190401C00271000";
+	//	System.out.println(s.substring(0, (s.length()-15)));	
+	//	System.out.println(s.charAt(s.length()-9));
 	}
 }
--- options/option.java
+++ options/option.java
@@ -7,23 +7,26 @@
 *"              " calculate greeks and IV
 *"              " get values
 **/
-package cader.options;
-
+package options;
+import java.util.*;
 
 
 public class option{
 
 	public final String contractSymbol, underlyingSymbol;
 	public final double strikePrice;
+	public final boolean isCallOption;	
 	private double mark, bid, ask, change, percentChange, volume, openInterest, delta, theta, gamma, vega, rho, iv;
-	
 	/**
 	* @param incomingSymbol The OCC symbol for the options contract represented by this class
 	* @see <a href="https://en.wikipedia.org/wiki/Option_symbol">Options symbol</a>
 	*/
 	public option(String incomingSymbol){
 		contractSymbol = incomingSymbol; 
-		underlyingSymbol = incomingSymbol.substring(0,(incomingSymbol.length()-17));
+		underlyingSymbol = incomingSymbol.substring(0,(incomingSymbol.length()-15));
+
+		isCallOption = (contractSymbol.toUpperCase().charAt(contractSymbol.length()-9) == 'C');
+		strikePrice = Double.parseDouble(contractSymbol.substring(contractSymbol.length()-8)) / 1000 ;
 		
 	}
 	/** @return  3 element array of type double containing the contract's bid, ask, and mark prices */
