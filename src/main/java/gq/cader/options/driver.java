2d897d2c3b887362ea3a46986162942de7a65f4a
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-02 10:35:42 -0700

    Either Hot Shit or Total Bollocks

--- src/main/java/gq/cader/options/driver.java
+++ src/main/java/gq/cader/options/driver.java
@@ -6,7 +6,9 @@ import yahoofinance.*;
 public class driver{
 
 	public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException{
-		if (args.length == 1){	
+
+		//ghetto test to see if arg[0] is an option symbol else if arg[0] is an equity symbol or no args
+		if (args.length == 1 && args[0].length() > 6){	
 			option o = new option(args[0]);
 			System.out.println(o.underlyingSymbol);
 			System.out.println(o.strikePrice);
@@ -14,11 +16,12 @@ public class driver{
 			System.out.println(o.expiration);	
 			System.out.println("Is Call Option:" + o.isCallOption);
 
-
+		}
+		if (args.length == 1 && args[0].length() < 6){
 			YahooFinance f = new YahooFinance();
 			while(true){
 
-				Stock stock = f.get(o.underlyingSymbol);
+				Stock stock = f.get(args[0]);
 				BigDecimal price = stock.getQuote().getPrice();
 				BigDecimal change = stock.getQuote().getChangeInPercent();
 				BigDecimal peg = stock.getStats().getPeg();
@@ -26,9 +29,9 @@ public class driver{
 				stock.print();
 				Thread.sleep(1000);
 				clearScreen();
-
 			}
 		}
+
 	}
 	public static void clearScreen() {  
 		System.out.print("\033[H\033[2J");  
