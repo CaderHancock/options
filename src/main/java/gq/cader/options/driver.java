109ed3f9ddf3573566a5af26f9827c273009a9b1
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-02 07:41:56 -0700

    hmmm

--- src/main/java/gq/cader/options/driver.java
+++ src/main/java/gq/cader/options/driver.java
@@ -6,21 +6,21 @@ import yahoofinance.*;
 public class driver{
 
 	public static void main(String[] args) throws java.io.IOException{
-		if (args.length == 9){	
+		if (args.length == 1){	
 			option o = new option(args[0]);
 			System.out.println(o.underlyingSymbol);
 			System.out.println(o.strikePrice);
 			//	String s = "SPY190401C00271000";
 			System.out.println(o.expiration);	
 			System.out.println("Is Call Option:" + o.isCallOption);
-		}
+		
 
 		YahooFinance f = new YahooFinance();
-		Stock stock = f.get(args[0]);
+		Stock stock = f.get(o.underlyingSymbol);
 		BigDecimal price = stock.getQuote().getPrice();
 		BigDecimal change = stock.getQuote().getChangeInPercent();
 		BigDecimal peg = stock.getStats().getPeg();
 		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
 		stock.print();
-	}
+	}}
 }
