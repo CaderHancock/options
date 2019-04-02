44b9e7c491b0830d4b0cdfcf8dc8d12b65a7c3f0
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-02 08:36:43 -0700

    looped driver to see realtime quotes

--- src/main/java/gq/cader/options/driver.java
+++ src/main/java/gq/cader/options/driver.java
@@ -5,7 +5,7 @@ import yahoofinance.*;
 
 public class driver{
 
-	public static void main(String[] args) throws java.io.IOException{
+	public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException{
 		if (args.length == 1){	
 			option o = new option(args[0]);
 			System.out.println(o.underlyingSymbol);
@@ -13,14 +13,26 @@ public class driver{
 			//	String s = "SPY190401C00271000";
 			System.out.println(o.expiration);	
 			System.out.println("Is Call Option:" + o.isCallOption);
-		
 
-		YahooFinance f = new YahooFinance();
-		Stock stock = f.get(o.underlyingSymbol);
-		BigDecimal price = stock.getQuote().getPrice();
-		BigDecimal change = stock.getQuote().getChangeInPercent();
-		BigDecimal peg = stock.getStats().getPeg();
-		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-		stock.print();
-	}}
+
+			YahooFinance f = new YahooFinance();
+			while(true){
+
+				Stock stock = f.get(o.underlyingSymbol);
+				BigDecimal price = stock.getQuote().getPrice();
+				BigDecimal change = stock.getQuote().getChangeInPercent();
+				BigDecimal peg = stock.getStats().getPeg();
+				BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+				stock.print();
+				Thread.sleep(1000);
+				clearScreen();
+
+			}
+		}
+	}
+	public static void clearScreen() {  
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();  
+	} 
+
 }
