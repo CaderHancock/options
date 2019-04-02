05ee0452ca6b687dde9a85edbbe3ced730d40305
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-02 07:31:24 -0700

    Good news! the 3rd party api works great! too bad i didn't read the docs and its useless.

--- src/main/java/gq/cader/options/driver.java
+++ src/main/java/gq/cader/options/driver.java
@@ -1,13 +1,26 @@
+
 package gq.cader.options;
+import java.math.*;
+import yahoofinance.*; 
 
 public class driver{
 
-public static void main(String[] args){
-	option o = new option(args[0]);
-	System.out.println(o.underlyingSymbol);
-	System.out.println(o.strikePrice);
-	//	String s = "SPY190401C00271000";
-		System.out.println(o.expiration);	
-		System.out.println("Is Call Option:" + o.isCallOption);
+	public static void main(String[] args) throws java.io.IOException{
+		if (args.length == 9){	
+			option o = new option(args[0]);
+			System.out.println(o.underlyingSymbol);
+			System.out.println(o.strikePrice);
+			//	String s = "SPY190401C00271000";
+			System.out.println(o.expiration);	
+			System.out.println("Is Call Option:" + o.isCallOption);
+		}
+
+		YahooFinance f = new YahooFinance();
+		Stock stock = f.get(args[0]);
+		BigDecimal price = stock.getQuote().getPrice();
+		BigDecimal change = stock.getQuote().getChangeInPercent();
+		BigDecimal peg = stock.getStats().getPeg();
+		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+		stock.print();
 	}
 }
