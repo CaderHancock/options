c4807312380310095637811a8042427168a29800
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-04 13:56:04 -0700

    Added another dependency

--- src/main/java/gq/cader/options/util/MultiLinePrinter.java
+++ src/main/java/gq/cader/options/util/MultiLinePrinter.java
@@ -1,5 +1,4 @@
 package gq.cader.options;
-
 import yahoofinance.*;
 import java.util.*;
 import com.googlecode.lanterna.SGR;
@@ -16,20 +15,23 @@ import com.googlecode.lanterna.screen.*;
 import com.googlecode.lanterna.TerminalPosition;
 public class MultiLinePrinter {
 	public MultiLinePrinter(List<String> lines, Screen screen, TerminalPosition startPos)throws java.io.IOException
-
 	{
-		TerminalPosition workingPos = new TerminalPosition(startPos.getColumn(), startPos.getRow());
-		TextGraphics tg = screen.newTextGraphics();
-		tg.setForegroundColor(TextColor.ANSI.WHITE);
-                tg.setBackgroundColor(TextColor.ANSI.BLACK);
-		for (int i = 0; i < lines.size(); i++)
+		try
 		{
-			tg.putString(workingPos, lines.get(i) );
-			workingPos = workingPos.withRelativeRow(1);
-		}	
-
-
-
-		screen.refresh();	
+			TerminalPosition workingPos = new TerminalPosition(startPos.getColumn(), startPos.getRow());
+			TextGraphics tg = screen.newTextGraphics();
+			tg.setForegroundColor(TextColor.ANSI.WHITE);
+                	tg.setBackgroundColor(TextColor.ANSI.BLACK);
+			for (int i = 0; i < lines.size(); i++)
+			{
+				tg.putString(workingPos, lines.get(i) );
+				workingPos = workingPos.withRelativeRow(1);
+			}	
+			screen.refresh();	
+		}catch(java.io.IOException e){
+			screen.close();
+			System.out.println("IOException in MultiLinePrinter\n\n");
+			e.printStackTrace();
+		}
 	}
 }
