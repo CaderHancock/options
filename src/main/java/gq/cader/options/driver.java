7f318b4322e3ba3ad843a275fdfe8e17ed9026f9
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-05 17:56:58 -0700

    YAY PROGRESS

--- src/main/java/gq/cader/options/driver.java
+++ src/main/java/gq/cader/options/driver.java
@@ -10,6 +10,7 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.terminal.TerminalResizeListener;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.screen.*;
@@ -17,21 +18,25 @@ import java.io.IOException;
 
 public class driver {
 	public static void main(String[] args)throws java.io.IOException
-	{
+	{	
 		boolean itsPartyTime = true;
+		YahooFinance f = new YahooFinance();
+		Thread currentFunction = null;
 		Screen screen = new DefaultTerminalFactory().createScreen();
 		screen.startScreen();
-		YahooFinance f = new YahooFinance();
 		TextGraphics textGraphics = screen.newTextGraphics();
 		TerminalSize terminalSize = screen.getTerminalSize();
 		int columns = terminalSize.getColumns();
 		int rows    = terminalSize.getRows();
+		final TerminalPosition searchBar = new TerminalPosition(1,5);
 		textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
 //		textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
 		textGraphics.fill(' ');
 		screen.refresh();
 		//Begin Draw LOOP
 		KeyStroke keyStroke = null;
+		String input = "";
+		String cachedSearch = null;
 		drawHeader(textGraphics,screen, columns,rows);
 		while(itsPartyTime)
 		{
@@ -45,8 +50,30 @@ public class driver {
 
 			}
 			
-			new SearchFunction("tsla", screen, Function.infoScreenTopLeft);
 			keyStroke = screen.readInput();
+
+			if(keyStroke.getKeyType() == KeyType.Escape)
+				itsPartyTime = false;
+
+			if(keyStroke.getKeyType() == KeyType.Character && input.length()<11)
+			{
+				input = input.concat(Character.toString(keyStroke.getCharacter()));
+				textGraphics.putString(searchBar, input	);
+			}
+			if(keyStroke.getKeyType() == KeyType.Backspace && input.length()>0)
+			{	
+				input = input.substring(0 , input.length()-1);
+				textGraphics.drawLine(searchBar, searchBar.withRelativeColumn(11), ' ');
+				textGraphics.putString(searchBar, input	);
+			}
+			if(keyStroke.getKeyType() == KeyType.Enter)
+			{
+				cachedSearch = input;
+				input = "";
+				textGraphics.drawLine(searchBar, searchBar.withRelativeColumn(11), ' ');
+				new SearchFunction(cachedSearch, screen, Function.infoScreenTopLeft );
+			}
+			screen.refresh();
 		} 
 	}
 
