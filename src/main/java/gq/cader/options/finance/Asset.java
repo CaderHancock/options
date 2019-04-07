fe40340b661252d61e1be621b2d0af10e7c3d9c2
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-07 16:22:18 -0700

    Refactored tests and added new test in default fail state

---
+++ src/main/java/gq/cader/options/finance/Asset.java
@@ -0,0 +1,11 @@
+package gq.cader.options;
+
+interface TradeableAsset{
+
+String symbol = null;
+double price = 0;
+
+
+}
+
+
--- src/test/java/gq/cader/options/optionsTest.java
+++ src/test/java/gq/cader/options/optionsTest.java
@@ -3,22 +3,35 @@ package gq.cader.options;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.Mockito.*;
 
 class optionsTest{
 
+	StockOption so;
+	@BeforeEach
+	void setup() {
+	
+	so = new OptionFactory("APTI190405C00185000").newStockOption();
+	}
 	@Test
 	public void testOptionFactoryWithStockOption(){
 		
-		StockOption so = new OptionFactory("AAPL190405C00185000").newStockOption();
 		assertNotNull(so);
 
 	}
 	@Test
 	public void testOpFacStockOptionFields(){
-	StockOption so = new OptionFactory("AAPL190405C00185000").newStockOption();
 	assertNotNull(so.getUnderlyingSymbol());
-	assertTrue(so.getUnderlyingSymbol().equals("AAPL"));
+	assertTrue(so.getUnderlyingSymbol().equals("APTI"));
 	assertTrue(so.isCallOption);
+	assertTrue(so.getStrikePrice() == 185);
 	}
+	@Test
+	public void testStockOptionBoolTypes(){
+		fail();	
+	
+	}
+
+
 }
