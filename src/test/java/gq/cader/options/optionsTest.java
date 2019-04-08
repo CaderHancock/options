3e431fe69d43e4a7f33c14f9ae6c6dae7aae9aaf
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-07 17:20:29 -0700

    More test stubs

--- src/test/java/gq/cader/options/optionsTest.java
+++ src/test/java/gq/cader/options/optionsTest.java
@@ -12,39 +12,70 @@ class optionsTest{
 	StockOption so;
 	@BeforeEach
 	void setup() {
-	
-	so = new OptionFactory("APTI190405C00185000").newStockOption();
+
+		so = new OptionFactory("APTI190405C00185000").newStockOption();
+		//co = new OptionFactory("").newCommoditiesOption();
+		//fo = new OptionFactory("").newForexOption();
+
 	}
 	@Test
 	public void testOptionFactoryWithStockOption(){
-		
+
 		assertNotNull(so);
 
 	}
 	@Test
 	public void testOpFacStockOptionFields(){
-	assertNotNull(so.getUnderlyingSymbol());
-	assertTrue(so.getUnderlyingSymbol().equals("APTI"));
-	assertTrue(so.isCallOption);
-	assertTrue(so.getStrikePrice() == 185);
+		assertNotNull(so.getUnderlyingSymbol());
+		assertTrue(so.getUnderlyingSymbol().equals("APTI"));
+		assertTrue(so.isCallOption);
+		assertTrue(so.getStrikePrice() == 185);
 	}
 	@Test
 	public void testStockOptionBoolTypes(){
-		
+
 		assertTrue(so.isStockOption);
 		assertFalse(so.isForexOption&&so.isCommoditiesOption);	
 	}
 	//TODO Find more illegal cases
 	@Test
 	public void testValidateStockOptionSymbol(){
-		
+
 
 		assertThrows(IllegalFormatException.class, () -> {
 			new OptionFactory("XXXXXXXXXXXXXXXX");});
 		assertThrows(IllegalFormatException.class, () -> {
 			new OptionFactory("APTI190405X00185000");});
-		assertThrows(IllegalFormatException.class, ("0000190405C00185000") -> {
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("0000190405C00185000");});
+
+
+
+	}//TODO Find more illegal cases
+	@Test
+	public void testValidateForexOptionSymbol(){
+
+
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("");});
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("");});
+		assertThrows(IllegalFormatException.class, () -> {
 			new OptionFactory();});
+
+
+
+	}//TODO Find more illegal cases
+	@Test	
+	public void testValidateCommoditiesOptionSymbol(){
+
+
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("");});
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("");});
+		assertThrows(IllegalFormatException.class, () -> {
+			new OptionFactory("");});
 
 
 
