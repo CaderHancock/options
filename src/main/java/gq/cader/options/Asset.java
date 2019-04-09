c1f17be929c1992606b0469b5e96fb332f86107c
Cader Hancock <Cader.Hancock@gmail.com>
2019-04-09 00:58:17 +0000

    basic stubs for Asset and Stock

--- src/main/java/gq/cader/options/Asset.java
+++ src/main/java/gq/cader/options/Asset.java
@@ -1,10 +1,19 @@
 package gq.cader.options;
+import java.util.*;
 
-interface Asset{
+abstract class Asset{
 
-String symbol = null;
-double value = 0;
+String symbol;
+private double value = 0;
 
+/**This default method throws an Exception so that i can extend all relevant classes before implementing the data API
+ * but i want to make sure that the default method is never called because each asset is going to have a different api call.
+ * TODO Abstract this method when data API integrated
+ */
+public double getValue(){
+throw new Exception();
+return value;
+}
 
 }
 
---
+++ src/main/java/gq/cader/options/Stock.java
@@ -0,0 +1,14 @@
+package gq.cader.options;
+
+
+class Stock extends Asset{
+
+	public Stock(String s){
+	
+	symbol = s;
+	}
+	public double getValue(){
+	//TODO api calls 
+	return value;
+	}
+}
