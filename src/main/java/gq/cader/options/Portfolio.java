f74e4b76a83738178a894a4f315ba93f036941cd
Cader Hancock <Cader.Hancock@gmail.com>
2019-06-03 17:59:17 +0000

    refuckulated the carbonator

--- src/main/java/gq/cader/options/Portfolio.java
+++ src/main/java/gq/cader/options/Portfolio.java
@@ -1,9 +1,15 @@
 package gq.cader.options;
 import java.util.Set;
+import java.util.stream.*;
 
-abstract class Portfolio extends Asset{
+class Portfolio extends Asset{
 
-private Set<Asset> portfolio;
-abstract public double getValue();
-abstract public Set<Asset> getAssets();
+	private Set<Asset> portfolio;
+	public double getValue(){
+	return portfolio.stream().mapToDouble(x -> x.getValue()).sum();
+}
+public Set<Asset> getAssets()
+{
+	return portfolio;
+}
 }
