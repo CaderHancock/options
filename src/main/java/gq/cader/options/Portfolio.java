bc589fbb83899969f6f5756186e87be1cd6a2e86
Cader Hancock <Cader.Hancock@gmail.com>
2019-06-03 20:26:44 +0000

    Constructors for portfolio

--- src/main/java/gq/cader/options/Portfolio.java
+++ src/main/java/gq/cader/options/Portfolio.java
@@ -1,9 +1,19 @@
 package gq.cader.options;
 import java.util.Set;
+import java.util.TreeSet;
 
 class Portfolio extends Asset{
 
 	private Set<Asset> portfolio;
+	
+	public Portfolio(){
+		portfolio = new TreeSet<Asset>();
+	}
+	public Portfolio(Set<Asset> portfolio){
+		this.portfolio = portfolio;
+	}
+
+	@Override
 	public double getValue(){
 		return portfolio.stream()
 				.mapToDouble(x -> x.getValue())
