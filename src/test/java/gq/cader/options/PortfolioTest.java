96a021a19bc04ad7f0b0b1d4a20687d127c98e80
Cader Hancock <cader.hancock@gmail.com>
2019-06-15 09:52:43 -0700

    Never before had a small typo like this one caused so much damage.

--- src/test/java/gq/cader/options/PortfolioTest.java
+++ src/test/java/gq/cader/options/PortfolioTest.java
@@ -1,25 +1,26 @@
 package gq.cader.options;
 
- import java.util.HashMap;
- import static org.junit.jupiter.api.Assertions.*;
- import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.converter.JavaTimeConversionPattern;
 import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.Mockito.*;
 import java.util.ArrayList;
- class PortfolioTest{
-	
-	 private Portfolio p;
-	 private Portfolio p1;
-	 private Stock stock, stock1;
-	 @BeforeEach
-	 void setup(){
-		try{stock = new AssetFactory("AAPL").getStock();
-		stock1 = new AssetFactory("AMZN").getStock();
-		HashMap<String, Asset> hm = new HashMap<String, Asset>();
-		hm.put(stock.getSymbol(), stock);
-		hm.put(stock1.getSymbol(), stock1);
-		p = new Portfolio(hm);
+class PortfolioTest{
+
+	private Portfolio p;
+	private Portfolio p1;
+	private Stock stock, stock1;
+	@BeforeEach
+	void setup(){
+		try{
+			stock = new AssetFactory("AAPL").getStock();
+			stock1 = new AssetFactory("AMZN").getStock();
+			HashMap<String, Asset> hm = new HashMap<String, Asset>();
+			hm.put(stock.getSymbol(), stock);
+			hm.put(stock1.getSymbol(), stock1);
+			p = new Portfolio(hm);
 		}catch (Exception e){}
 		Stock mock1 = mock(Stock.class);
 		Stock mock2 = mock(Stock.class);
@@ -29,16 +30,16 @@ import java.util.ArrayList;
 		mocking.put("foo", mock1);
 		mocking.put("bar", mock2);
 		p1 = new Portfolio(mocking);
-	 }
-	 @Test
-	 public void yolo(){
-		 assertTrue(p1.getValue()==420.0);
-		 System.out.println(p1.getValue());
-		 System.out.println(stock.getValue());
-		 System.out.println(stock1.getValue());
-		 // AssertTrue(p.getValue()>0);
-	 }
+	}
+	@Test
+	public void yolo(){
+		assertTrue(p1.getValue()==420.0);
+		System.out.println(p1.getValue());
+		System.out.println(stock.getValue());
+		System.out.println(stock1.getValue());
+		// AssertTrue(p.getValue()>0);
+	}
 
 
 
- }
+}
