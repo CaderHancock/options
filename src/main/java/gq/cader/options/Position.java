dd5a67fa933d65e3703ef427c8b6452f91098edc
Cader Hancock <cader.hancock@gmail.com>
2019-08-11 15:47:33 +0000

    Committing in accordance with the prophecy.

--- src/main/java/gq/cader/options/Position.java
+++ src/main/java/gq/cader/options/Position.java
@@ -3,6 +3,7 @@ package gq.cader.options;
 class Position extends Asset{
 	Asset asset;
 	int quantity = 0;
+	double averageCost;
 	Position(Asset asset, int quantity){
 		this.symbol = asset.getSymbol();
 		this.quantity = quantity;
